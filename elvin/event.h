8f43818325e77dd1b52a82dd379a4896a260b446
Joe Turner <joe@oampo.co.uk>
2013-02-26 14:46:37 +0000

    Add code to unpack patterns into pattern event callback

--- elvin/event.h
+++ elvin/event.h
@@ -8,6 +8,7 @@
 #include "scheduler.h"
 #include "pattern.h"
 #include "time_data.h"
+#include "indices.h"
 
 using namespace thelonious;
 
@@ -46,7 +47,13 @@ template <size_t numberOfPatterns, typename CallbackType>
 class PatternEvent : public Event {
 public:
     virtual bool process(const TimeData &time) {
-        // Work out how to run callback here!
+
+        bool shouldContinue = runCallback();
+        if (!shouldContinue) {
+            // Pattern hasn't provided a value, so don't process the callback
+            // or add any further events
+            return false;
+        }
 
         NextTuple tuple = durationPattern->next();
         Sample value = std::get<0>(tuple);
@@ -62,6 +69,35 @@ public:
     std::array<Pattern, numberOfPatterns> patterns;
     Pattern durationPattern;
     CallbackType callback;
+
+private:
+    // Runs the callback samples collected from the patterns
+    bool runCallback() {
+        // Collect the next value from each of the patterns
+        std::array<Sample, numberOfPatterns> arguments;
+        for (int i=0; i<numberOfPatterns; i++) {
+            NextTuple tuple = patterns[i]->next();
+
+            bool hasValue = std::get<1>(tuple);
+            if (!hasValue) {
+                return false;
+            }
+
+            arguments[i] = std::get<0>(tuple);
+        }
+
+        // Run the callback using an index pack
+        call(arguments, BuildIndices<numberOfPatterns>{});
+        return true;
+    }
+
+    // Template magic to expand the array into arguments for the callback.
+    // Expansion is done using an index pack.  I really don't understand how
+    // this works :D
+    template <size_t... I>
+    void call(std::array<Sample, numberOfPatterns> &arguments, indices<I...>) {
+        callback(arguments[I]...);
+    }
 };
 
 bool compareEventTime(std::unique_ptr<Event> &a, std::unique_ptr<Event> &b) {
