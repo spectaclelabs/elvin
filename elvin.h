262a374d9263eef6c7d2c4da4489b55c68e26cd2
Joe Turner <joe@oampo.co.uk>
2013-02-26 15:43:28 +0000

    Include files in elvin.h

--- elvin.h
+++ elvin.h
@@ -1,1 +1,12 @@
+#ifndef ELVIN_H
+#define ELVIN_H
+
+#include "elvin/pattern.h"
+#include "elvin/event.h"
 #include "elvin/scheduler.h"
+
+#include "elvin/pattern.h"
+#include "elvin/p_random.h"
+#include "elvin/p_sequence.h"
+
+#endif
