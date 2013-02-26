138aa61655cd9187367c44e2c65c42a0c991c6ea
Joe Turner <joe@oampo.co.uk>
2013-02-26 16:08:59 +0000

    Add compile test files

---
+++ tests/test_scheduler.cc
@@ -0,0 +1,9 @@
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+#include "elvin/scheduler.h"
+
+int main(int argc, char** argv) {
+    testing::InitGoogleMock(&argc, argv);
+    return RUN_ALL_TESTS();
+}
